b2f131b445dfaf2daac9ca4a87a19a061077c136
Michael Stensby <michael@codecov.io>
2021-08-26 14:34:12 -0600

    Add code, tests and github actions config

---
+++ app/tests/app_tests.py
@@ -0,0 +1,13 @@
+
+import unittest
+
+import app
+
+
+class TestMethods(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(app.smile(), ":)")
+
+
+if __name__ == '__main__':
+    unittest.main()
